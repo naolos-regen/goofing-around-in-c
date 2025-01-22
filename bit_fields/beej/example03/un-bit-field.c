310d11a5222d19fcead1dec9c5f1e4df9dc2441b
naolos-regen <naolos.regen@proton.me>
2025-01-22 19:00:23 +0100

    checked objdump removed asm

--- bit_fields/beej/example03/un-bit-field.c
+++ bit_fields/beej/example03/un-bit-field.c
@@ -43,7 +43,7 @@ struct bar2
 			  // comparing that is tough, but comapring the insides of struct is the same
 			  // in a sense of comparing the same insides with same insides, it wont provide me the nessesary
 			  // comparison.
-			  // however I'll checkout the objdump
+
 };
 
 int	compare_structs(const struct bar *bar1, const struct bar2 *bar2)
