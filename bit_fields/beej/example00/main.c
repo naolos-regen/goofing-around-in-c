85a80e65598ff2dee7e99888c2a3f9a2b859a6ed
naolos-regen <naolos.regen@proton.me>
2025-01-22 17:36:51 +0100

    beej example

--- bit_fields/beej/example00/main.c
+++ bit_fields/beej/example00/main.c
@@ -8,7 +8,18 @@ struct foo
 	unsigned int d;
 };
 
+struct foo2
+{
+	unsigned int a:1;
+	unsigned int b:1;
+	unsigned int c:1;
+	unsigned int d:1;
+};
+
 int 	main(void)
 {
 	printf("%zu\n", sizeof(struct foo));
+	printf("%zu\n", sizeof(struct foo2));
+
+	return (0);
 }
