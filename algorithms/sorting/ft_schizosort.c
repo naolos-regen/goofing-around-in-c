957a96939ef5e7653ad413c8b1414c9cc9029ddd
naolos-regen <naolos.regen@proton.me>
2025-02-01 14:00:13 +0100

    #embed isn't implemented yet?

--- algorithms/sorting/ft_schizosort.c
+++ algorithms/sorting/ft_schizosort.c
@@ -1,4 +1,27 @@
+#include <stdio.h>
+
+// #embed feature is not implemented yet?
 void	ft_shizosort(void * list)
-{		
-	list = (void*)0;
+{	
+	int shizomoment[] =
+	{ 
+		#embed "/dev/urandom" limit(10) 
+	};
+	int cx;
+	list = shizomoment;
+}
+
+
+
+int	main(void)
+{
+	int list[] = { 1, 2, 3, 4, 5 };
+	ft_shizosort(list);
+		
+	for (int i = 0 ; i < 4; i++)
+	{
+		printf("%d", list[i]);
+	}
+
+	return (0);
 }
